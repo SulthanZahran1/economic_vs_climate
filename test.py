a554f73bec1f4fbcd6fbc0da80c88cef5a8f33e4
SulthanZahran1 <zsulthan9@gmail.com>
2023-08-30 16:22:37 +0700

    asd

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-import xarray
+import xarray as xr
+import matplotlib.pyplot as plt
+
+# Load the NetCDF file
+data = xr.open_dataset('raw_data/gistemp1200_GHCNv4_ERSSTv5.nc')
+# Assuming the variable name is 'temperature' and you want the data for a specific timestamp
+
